e4c9abf16bcac13b72170ae2b244b8ea658561ea
GitHub Action <action@github.com>
2021-05-21 02:16:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/hqweb/androidtemplat/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/hqweb/androidtemplat/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hqweb.androidtemplat.listeners
 
+import com.github.hqweb.androidtemplat.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/hqweb/androidtemplat/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hqweb/androidtemplat/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hqweb.androidtemplat.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hqweb.androidtemplat.MyBundle
 
 class MyApplicationService {
 
